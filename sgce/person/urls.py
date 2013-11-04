123088ad8ec5e1096757b5e9e1435da39debfee0
Ramon Saraiva <ramonsaraiva@gmail.com>
2013-11-04 19:17:09 -0200

    added login and logout views

--- sgce/person/urls.py
+++ sgce/person/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.views import login, logout
 from person import views
 
 urlpatterns = patterns('',
+    url(r'^login/$', login, {'template_name': 'person/login.html'}, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/person/login/'}, name='logout'),
     url(r'^signup/$', views.PersonCreate.as_view(), name='signup'),
 )
