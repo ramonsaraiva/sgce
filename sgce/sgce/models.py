805ea747b9b12e440fb285d34be788898f685bb4
Ramon Saraiva <ramonsaraiva@gmail.com>
2013-11-05 21:15:15 -0200

    added the scope of event and activity models, need to write the decimal values..

---
+++ sgce/sgce/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Event(models.Model):
+	name = models.CharField(verbose_name='Nome', max_length=32)
+	description = models.CharField(verbose_name='Descrição', max_length=256)
+	date = models.DateField(verbose_name='Data')
+	time = models.TimeField(verbose_name='Hora')
+	local = models.CharField(verbose_name='Local', max_length=64)
+	address = models.CharField(verbose_name='Endereço', max_length=512)
+	maxpeople = models.DecimalField(verbose_name='Vagas',..)
+	nvouchers = models.DecimalField(verbose_name='Vouchers',..)
+
+	def __unicode__(self):
+		return self.name
+
+class Activity(models.Model):
+	name = models.CharField(verbose_name='Nome', max_length=32)
+	description = models.CharField(verbose_name='Descrição', max_length=256)
+	date = models.DateField(verbose_name='Data')
+	time = models.TimeField(verbose_name='Hora')
+	local = models.CharField(verbose_name='Local', max_length=64)
+	address = models.CharField(verbose_name='Endereço', max_length=512)
+	maxpeople = models.DecimalField(verbose_name='Vagas',..)
+	charge = models.DecimalField(verbose_name='Carga Horária', ..)
+	points = models.DecimalField(verbose_name='Pontos para participante', ..)
+	price = models.DecimalField(verbose_name='Price', ..)
+	accept_group_off = models.BooleanField(verbose_name='Aceita desconto por grupo')
+	accept_voucher = models.BooleanField(verbose_name='Aceita Voucher')
+
+	def __unicode__(self):
+		return self.name
