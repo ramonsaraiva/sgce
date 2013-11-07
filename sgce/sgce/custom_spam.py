a0f9f980a79cd6d5ce9cc2f03828b4816efdd909
Ramon Saraiva <ramonsaraiva@gmail.com>
2013-11-07 11:16:10 -0200

    added names to the db spam and new spam lib functions, also writed files for clearing db 'not user tables' and to ignite the spam

--- sgce/sgce/custom_spam.py
+++ sgce/sgce/custom_spam.py
@@ -1,5 +1,28 @@
 from dilla import spam
+import names
 
 @spam.strict_handler('sgce.Event.slug')
 def blank_slug(record, field):
 	return None
+
+def gen_names(times):
+	n = ''
+	for i in range(times):
+		n += names.get_full_name() + ' '
+	return n
+
+@spam.strict_handler('sgce.Event.name')
+def event_name(record, field):
+	return gen_names(1)
+
+@spam.strict_handler('sgce.Event.description')
+def event_description(record, field):
+	return gen_names(10)
+
+@spam.strict_handler('sgce.Activity.name')
+def activity_name(record, field):
+	return gen_names(1)
+
+@spam.strict_handler('sgce.Activity.description')
+def activity_description(record, field):
+	return gen_names(10)
