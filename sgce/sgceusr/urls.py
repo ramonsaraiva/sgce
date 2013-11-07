01d53e9f3a18dc1564f5a7839371b6a8b5dbdace
Ramon Saraiva <ramonsaraiva@gmail.com>
2013-11-07 09:26:41 -0200

    added a templatetag for current tab and refactored the activities template

--- sgce/sgceusr/urls.py
+++ sgce/sgceusr/urls.py
@@ -4,5 +4,6 @@ from sgceusr import views
 urlpatterns = patterns('',
 	url(r'^$', 'sgceusr.views.home', name='home'),
 	url(r'^events/$', views.EventList.as_view(), name='events'),
-	url(r'^event/(?P<slug>[\w\-]+)$', views.EventDetail.as_view(), name='event'),
+	url(r'^events/(?P<slug>[\w\-]+)$', views.EventDetail.as_view(), name='event'),
+	url(r'^activities/$', views.ActivityList.as_view(), name='activities'),
 )
--- sgce/sgceusr/views.py
+++ sgce/sgceusr/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from sgce.models import Event
+from sgce.models import Event, Activity
 
 def home(request):
 	return render_to_response('sgceusr/home.html', RequestContext(request))
@@ -14,3 +14,7 @@ class EventList(ListView):
 class EventDetail(DetailView):
 	model = Event
 	template_name = 'sgceusr/event.html'
+
+class ActivityList(ListView):
+	model = Activity
+	template_name = 'sgceusr/activities.html'
